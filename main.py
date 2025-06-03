cd77a322a6b38291a28472e12436e0faeba10789
Arina <bagdaarina@gmail.com>
2025-06-03 11:35:08 +0500

    add command menu to bot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 
 from aiogram import Bot, Dispatcher
+from aiogram.types import BotCommand
 
 from bot_instance import bot
 from bot.handlers.user_handlers import user_router
@@ -14,18 +15,31 @@ def register_routers(dp: Dispatcher) -> None:
     dp.include_router(user_router)
 
 
+async def setup_bot_commands():
+    bot_commands = [
+        BotCommand(command="/start", description="Стартовать бота"),
+        BotCommand(command="/new_form", description="Создать новую доверенность"),
+        BotCommand(command="/done", description="Закончить загрузку документов")
+    ]
+    await bot.set_my_commands(bot_commands)   
+
+
 async def main(bot: Bot) -> None:
     """Entry point for the bot."""
 
     config = BotConfig(
-        admin_ids=[123456789, 1305675],  
-        welcome_message="Welcome to the bot!"
+        admin_ids=[1305675],  
+        welcome_message="Здравст"
     )
 
     dp = Dispatcher()
     dp['config'] = config
 
     register_routers(dp)
+    
+    # Set up bot commands
+    await setup_bot_commands()
+    
     await dp.start_polling(bot)
 
     
